543ad2262da494978eeb28acc494f459f4b3868e
Moth Harper <103866827+kokofruit@users.noreply.github.com>
2025-02-11 00:30:58 -0500

    ReelC outputs to PointerC

--- Assets/Scripts/PointerController.cs
+++ Assets/Scripts/PointerController.cs
@@ -2,68 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using static UnityEditor.PlayerSettings;
 using static UnityEngine.GraphicsBuffer;
 
 public class PointerController : MonoBehaviour
 {
     public static PointerController instance;
+    [SerializeField] float pointerMoveSpeed;
 
-    // The range bar that limits where the target can move in
-    [SerializeField] Image rangeBar;
-    // The width of the rangeBar, which is the range of the pointer
-    float range;
-
-    // The target that moves around
-    [SerializeField] Image target;
-
-    // The RectTransform of the pointer
-    RectTransform rectTransform;
-    // The y position of the pointer
-    float yPos;
-    // The left-most and right-most positions of the pointer
-    float minPos;
-    float maxPos;
-    // The speed of the pointer
-    [SerializeField] float moveSpeed;
-
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         instance = this;
-
-        //// Store the rect transform of the pointer
-        rectTransform = GetComponent<RectTransform>();
-
-        //// Store the range of the fish bar
-        //range = rangeBar.GetComponent<RectTransform>().rect.width;
-
-        //// Store the left-most position of the pointer
-        //minPos = rectTransform.TransformPoint(rectTransform.position).x;
-
-
-        //// Calculate the right-most position of the fish bar
-        //maxPos = minPos + range;
-
-        RectTransform rangeRectTransform = rangeBar.GetComponent<RectTransform>();
-
-        range = rangeRectTransform.rect.width;
-
-        minPos = rangeRectTransform.position.x - (range / 2);
-
-        print(range + ", " + minPos + ", " + maxPos);
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        // do something
-        
     }
 
     public void MovePointer(float position)
     {
-        float newX = Mathf.MoveTowards(rectTransform.position.x, position * range + minPos, moveSpeed * Time.deltaTime);
-        rectTransform.position = new Vector2(newX, rectTransform.position.y);
+        // Clamp the position
+        //float cPosition = Mathf.Clamp(position, 0, maxTurnSpeed);
+        float cPosition = Mathf.Clamp01(position);
+        // Find the placement along the range bar
+        float rPosition = cPosition - 0.5f;
+        // Limit the movement by pointer speed 
+        float xPosition = Mathf.MoveTowards(transform.localPosition.x, rPosition, pointerMoveSpeed * Time.deltaTime);
+        // Set the pointer's position
+        transform.localPosition = new Vector2(xPosition, 0);
     }
-    
 }
--- Assets/Scripts/ReelController.cs
+++ Assets/Scripts/ReelController.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 
 public class ReelController : MonoBehaviour
 {
-    // Serialized variables
-    // DEBUG
-    [SerializeField] bool DEBUG = false;
-    // The maximum turning speed
-    [SerializeField] float turnSpeed;
+        // Serialized variables //
+    // The maximum output turning speed
+    [SerializeField] float maxTurnSpeed;
 
-    // Private variables
+        // Private variables //
     // The main camera of the scene
     Camera cam;
-    // The last rotation of the reel
-    float lastRot = 0f;
-    // The rigidbody2d of the reel
-    Rigidbody2D rb;
-    // The fish bar manager
-    FishBarManager fishBarManager;
-
 
     // Initialize variables
     private void Start()
     {
         cam = Camera.main;
-        rb = GetComponent<Rigidbody2D>();
-        fishBarManager = FindFirstObjectByType<FishBarManager>();
     }
 
     // Call helper functions
     private void FixedUpdate()
     {
+        // Rotate the reel to the mouse and calculate the rotation speed
         float speed = RotateReel();
         //float speed = MeasureSpeed();
-        PointerController.instance.MovePointer( speed / turnSpeed);
+        PointerController.instance.MovePointer(speed / maxTurnSpeed);
     }
 
     float RotateReel()
@@ -43,25 +33,13 @@ public class ReelController : MonoBehaviour
         Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         // Calculate the angle between the top of the reel and the mouse position
-        //float angle = Vector2.SignedAngle(Vector2.up, mousePos);
-        //float rot = Mathf.MoveTowardsAngle(rb.rotation, angle, turnSpeed * Time.deltaTime);
-
         float angle = Vector2.SignedAngle(transform.up, mousePos);
-        //float rot = Mathf.Clamp(angle, -1f * turnSpeed * Time.deltaTime, 0f);
 
         // Rotate the reel
         transform.Rotate(new Vector3(0, 0, angle));
-        //transform.Rotate(new Vector3(0,0,rot));
-        //rb.rotation = rot;
 
-        return -1f * angle;
+        // Return the rotation per second. Invert negative to account for counter/clockwise reel rotation.
+        return -1f * angle / Time.deltaTime;
     }
 
-    void MeasureSpeed()
-    {
-        // Find the mouse's position in the world
-        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        float angle = Vector2.SignedAngle(transform.up, mousePos);
-
-    }
 }
